ef2cfc93b332092d286ea6fe5748c280ecbf8b80
Bryan C Guner <bryan.guner@gmail.com>
2021-07-15 06:10:34 +0000

    update

--- InstantaneousGramme/app/forms/follow_form.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import HiddenField
-
-
-class FollowForm(FlaskForm):
-    follower_id = HiddenField("follower")
--- InstantaneousGramme/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .db import db
-from .user import User
-from .post import Post
-from .comment import Comment
-from .commentLike import CommentLike
-from .postLike import PostLike
-from .DM import DirectMessage
